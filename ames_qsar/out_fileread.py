2e07a7942fa3c80bee1eff87ddc8d0078f3ebeff
twsleight <twsleight@gmail.com>
2020-10-14 12:01:37 -0400

    removing whitespace and debugging code

--- ames_qsar/out_fileread.py
+++ ames_qsar/out_fileread.py
@@ -1,17 +1,11 @@
 import os
 import glob
 def out_fileread(filepath):
-
     os.chdir(filepath)
-    #filepath = '/home/twsleight/ames_qsar/tests'
-
-    #print(filepath + '/*.out')
     namelist = []
     for filename in glob.glob('*.out'):
 
-    #    print(filename)
         filename = filename.replace(filepath, '')
         namelist.append(filename)
-        print(namelist)
 
     return namelist
