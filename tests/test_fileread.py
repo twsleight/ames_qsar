21572771af7aa1e0f551a13ddf19bf1cd200b3ff
twsleight <twsleight@gmail.com>
2020-10-14 12:48:27 -0400

    corrected method of referencing local files for test_fileread.py

--- tests/test_fileread.py
+++ tests/test_fileread.py
@@ -1,12 +1,9 @@
-#adde the current working directory to the PATH
-import sys,os
-sys.path.append(os.getcwd())
-
 import pytest
 import os
 from ames_qsar.out_fileread import out_fileread
 
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 def test_fileread():
     #these are the test files that should be found
-    assert set(out_fileread(os.getcwd()+ '/tests')) == set(['coumarin_gas.out'\
+    assert set(out_fileread(THIS_DIR)) == set(['coumarin_gas.out'\
         ,'coumarin_neg.out','coumarin_pos.out' ])
