ff53251247df81cc135a53c2f3827825ad53dcf3
twsleight <twsleight@gmail.com>
2020-10-14 13:44:06 -0400

    correcting an old naming convention to change amesQSAR to ames_qsar

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open('requirements.txt', 'r') as f:
     requirements = f.read().splitlines()
 
 setup(
-   name='amesQSAR',
+   name='ames_qsar',
    version='1.0',
    description='small hydrocarbon QSAR for direct acting Ames test mutagens',
    author='Trevor Sleight',
    author_email='twsleight@gmail.com',
-   packages=['amesQSAR'],  #same as name
+   packages=['ames_qsar'],  #same as name
    install_requires=requirements, #external packages as dependencies
 )
