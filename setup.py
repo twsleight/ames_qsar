134c494d56cd15639bfa5828f663436c2b894f14
twsleight <twsleight@gmail.com>
2020-10-21 21:50:19 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+setup(
+   name='ames_qsar',
+   version='1.0',
+   description='small hydrocarbon QSAR for direct acting Ames test mutagens',
+   author='Trevor Sleight',
+   author_email='twsleight@gmail.com',
+   packages=['ames_qsar'],  #same as name
+   install_requires=requirements, #external packages as dependencies
+)
