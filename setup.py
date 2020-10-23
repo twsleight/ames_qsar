f07f58f42c7dd0a15e75cd23033ec537ebca8984
twsleight <twsleight@gmail.com>
2020-10-22 20:38:06 -0400

    updating setup.py to include the requirments

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='ames_qsar',
+   version='1.0',
+   description='small hydrocarbon QSAR for direct acting Ames test mutagens',
+   author='Trevor Sleight',
+   author_email='twsleight@gmail.com',
+   packages=['ames_qsar'],  #same as name
+   install_requires=['numpy~=1.19', 'pandas>=1.1'], #external packages as dependencies
+)
